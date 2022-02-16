9abbbbb40e6c8d94577a9cfa389ad0137397d71b
Zhong Jianxin <azuwis@gmail.com>
2022-02-16 23:53:13 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="pianotrans",
+    version="0.2.1",
+    author="Zhong Jianxin",
+    author_email="azuwis@gmail.com",
+    description="Simple GUI for ByteDance's Piano Transcription with Pedals",
+    py_modules=["PianoTrans"],
+    install_requires=[
+        'piano_transcription_inference',
+    ],
+    entry_points={
+        'console_scripts':[
+            'pianotrans = PianoTrans:main',
+        ],
+    },
+)
