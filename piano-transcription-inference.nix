a4143555c11d634aaa89b98c374e05ecd21640c0
Zhong Jianxin <azuwis@gmail.com>
2022-02-16 23:50:05 +0800

    nix: Add checkpoint for piano-transcription-inference

--- piano-transcription-inference.nix
+++ piano-transcription-inference.nix
@@ -2,6 +2,7 @@
 , lib
 , buildPythonPackage
 , fetchPypi
+, fetchurl
 , matplotlib
 , mido
 , librosa
@@ -17,12 +18,24 @@ buildPythonPackage rec {
     sha256 = "sha256-nbhuSkXuWrekFxwdNHaspuag+3K1cKwq90IpATBpWPY=";
   };
 
+  checkpoint = fetchurl {
+    name = "note_F1=0.9677_pedal_F1=0.9186.pth";
+    url = "https://zenodo.org/record/4034264/files/CRNN_note_F1%3D0.9677_pedal_F1%3D0.9186.pth?download=1";
+    sha256 = "sha256-w/qXMHJb9Kdi8cFLyAzVmG6s2gGwJvWkolJc1geHYUE=";
+  };
+
   propagatedBuildInputs = [
     matplotlib
     mido
     librosa
     torchlibrosa
   ];
+
+  postPatch = ''
+    substituteInPlace piano_transcription_inference/inference.py --replace \
+      "checkpoint_path='{}/piano_transcription_inference_data/note_F1=0.9677_pedal_F1=0.9186.pth'.format(str(Path.home()))" \
+      "checkpoint_path='${checkpoint}'"
+  '';
 
   doCheck = false;
 
