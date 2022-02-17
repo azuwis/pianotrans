33dbd287b70a3247f3c69fe85b95b4e8a35b69f5
Zhong Jianxin <azuwis@gmail.com>
2022-02-17 14:16:26 +0800

    nix: Fix overlays for Linux

--- flake.nix
+++ flake.nix
@@ -17,15 +17,21 @@
                   sha256 = "sha256-xT0F52iuSj5VOuIcLlAVKT5e+/1cEtSX8RBMtRnMprM=";
                 };
               });
-              mido = prev.mido.overrideAttrs (o: {
-                propagatedBuildInputs = if super.stdenv.isDarwin then [] else o.propagatedBuildInputs;
-              });
-              soundfile = prev.soundfile.overrideAttrs (o: {
-                patches = if (super.stdenv.system == "aarch64-darwin") then [ ./soundfile.patch ] else o.patches;
-                prePatch = if (super.stdenv.system == "aarch64-darwin") then ''
-                  rm tests/test_pysoundfile.py
-                '' else o.prePatch;
-              });
+              mido = prev.mido.overrideAttrs (o:
+                if super.stdenv.isDarwin
+                then { propagatedBuildInputs = []; }
+                else { }
+              );
+              soundfile = prev.soundfile.overrideAttrs (o:
+                if (super.stdenv.system == "aarch64-darwin")
+                then {
+                  patches = [ ./soundfile.patch ];
+                  prePatch = ''
+                    rm tests/test_pysoundfile.py
+                  '';
+                }
+                else { }
+              );
               torchlibrosa = self.python3.pkgs.callPackage ./torchlibrosa.nix {};
               piano-transcription-inference = self.python3.pkgs.callPackage ./piano-transcription-inference.nix {};
               pianotrans = self.python3.pkgs.callPackage ./pianotrans.nix { pytorch = self.python3.pkgs.pytorch-bin; };
