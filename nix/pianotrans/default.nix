58b4e6e0931196b21bdd6ec15dad111d5aef8bfb
Zhong Jianxin <azuwis@gmail.com>
2024-01-08 21:19:32 +0800

    nix: Use lib.fileset to set pianotrans src
    This make `nix run .#` get the current git version of PianoTrans.py

--- nix/pianotrans/default.nix
+++ nix/pianotrans/default.nix
@@ -1,19 +1,19 @@
 { lib
-, fetchFromGitHub
 , python3
 , ffmpeg
 }:
 
-python3.pkgs.buildPythonApplication rec {
+python3.pkgs.buildPythonApplication {
   pname = "pianotrans";
   version = "1.0.1";
   format = "setuptools";
 
-  src = fetchFromGitHub {
-    owner = "azuwis";
-    repo = pname;
-    rev = "v${version}";
-    hash = "sha256-gRbyUQmPtGvx5QKAyrmeJl0stp7hwLBWwjSbJajihdE=";
+  src = with lib.fileset; toSource {
+    root = ../../.;
+    fileset = unions [
+      ../../PianoTrans.py
+      ../../setup.py
+    ];
   };
 
   propagatedBuildInputs = with python3.pkgs; [
