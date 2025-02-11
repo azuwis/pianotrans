2bbeba47439210c336a116e3b9b0921f897a9b59
Zhong Jianxin <azuwis@gmail.com>
2025-02-11 21:01:21 +0800

    nix: Refactor and update
    - Update to nixos 24.11
    - Drop flake-utils

--- flake.nix
+++ flake.nix
@@ -1,12 +1,11 @@
 {
   inputs = {
-    # https://hydra.nix-community.org/jobset/nixpkgs/cuda
-    nixpkgs.url = "github:NixOS/nixpkgs/ac2df85f4d5c580786c7b4db031c199554152681";
+    # nixos-24.11-small cuda https://hydra.nix-community.org/jobset/nixpkgs/cuda-stable
+    nixpkgs.url = "github:NixOS/nixpkgs/a45fa362d887f4d4a7157d95c28ca9ce2899b70e";
     devshell = {
       url = "github:numtide/devshell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   nixConfig = {
@@ -22,85 +21,95 @@
 
   outputs =
     inputs@{ ... }:
-    inputs.flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import inputs.nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
-
-        pkgsCuda = import inputs.nixpkgs {
-          inherit system;
-          config = {
-            allowUnfree = true;
-            cudaSupport = true;
-          };
-        };
-
-        blas = "${pkgs.mkl}/lib/libblas.so";
-        python3-bin = pkgs.python3.override {
-          packageOverrides = self: super: { torch = super.torch-bin; };
-        };
-        python3-cuda = pkgsCuda.python3;
-
-        pianotrans = pkgs.callPackage ./nix/pianotrans { };
-        pianotrans-bin = pianotrans.override { python3 = python3-bin; };
-        pianotrans-cuda = pianotrans.override { python3 = python3-cuda; };
-        pianotrans-mkl =
-          let
-            inherit (pkgs) runCommand makeWrapper;
-          in
-          runCommand "pianotrans" { buildInputs = [ makeWrapper ]; } ''
-            makeWrapper ${pianotrans}/bin/pianotrans $out/bin/pianotrans \
-              --set LD_PRELOAD "${blas}"
-          '';
-
-        devshell = import inputs.devshell {
-          inherit system;
-          nixpkgs = pkgs;
-        };
-        mkShellPkgs = python: [
-          (python.withPackages (ps: [
-            ps.piano-transcription-inference
-            ps.resampy
-            ps.tkinter
-          ]))
-          pkgs.ffmpeg
-        ];
-
-        shell = devshell.mkShell { packages = mkShellPkgs pkgs.python3; };
-        shell-bin = devshell.mkShell { packages = mkShellPkgs python3-bin; };
-        shell-cuda = devshell.mkShell { packages = mkShellPkgs python3-cuda; };
-        shell-mkl = devshell.mkShell {
-          packages = mkShellPkgs pkgs.python3;
-          env = [
-            {
-              name = "LD_PRELOAD";
-              value = blas;
-            }
-          ];
-        };
-      in
-      {
-        packages = {
+    let
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      eachSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs systems (
+          system:
+          f rec {
+            inherit system;
+            devshell = import inputs.devshell { nixpkgs = pkgs; };
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              config.allowUnfree = true;
+            };
+            python3-bin = pkgs.python3.override {
+              packageOverrides = self: super: { torch = super.torch-bin; };
+            };
+            python3-cuda =
+              (import inputs.nixpkgs {
+                inherit system;
+                config = {
+                  allowUnfree = true;
+                  cudaSupport = true;
+                };
+              }).python3;
+          }
+        );
+    in
+    {
+      packages = eachSystem (
+        {
+          pkgs,
+          python3-bin,
+          python3-cuda,
+          ...
+        }:
+        rec {
           default = pianotrans;
-          inherit
-            pianotrans
-            pianotrans-bin
-            pianotrans-cuda
-            pianotrans-mkl
-            ;
-        };
-        devShells = {
+          pianotrans = pkgs.callPackage ./nix/pianotrans { };
+          pianotrans-bin = pianotrans.override { python3 = python3-bin; };
+          pianotrans-cuda = pianotrans.override { python3 = python3-cuda; };
+          pianotrans-mkl =
+            let
+              inherit (pkgs) runCommand makeWrapper;
+            in
+            runCommand "pianotrans" { buildInputs = [ makeWrapper ]; } ''
+              makeWrapper ${pianotrans}/bin/pianotrans $out/bin/pianotrans \
+                --set LD_PRELOAD "${pkgs.mkl}/lib/libblas.so"
+            '';
+        }
+      );
+
+      devShells = eachSystem (
+        {
+          pkgs,
+          devshell,
+          python3-bin,
+          python3-cuda,
+          ...
+        }:
+        let
+          mkShellPkgs = python: [
+            (python.withPackages (ps: [
+              ps.piano-transcription-inference
+              ps.resampy
+              ps.tkinter
+            ]))
+            pkgs.ffmpeg
+          ];
+        in
+        rec {
           default = shell;
-          inherit
-            shell
-            shell-bin
-            shell-cuda
-            shell-mkl
-            ;
-        };
-      }
-    );
+          shell = devshell.mkShell { packages = mkShellPkgs pkgs.python3; };
+          shell-bin = devshell.mkShell { packages = mkShellPkgs python3-bin; };
+          shell-cuda = devshell.mkShell { packages = mkShellPkgs python3-cuda; };
+          shell-mkl = devshell.mkShell {
+            packages = mkShellPkgs pkgs.python3;
+            env = [
+              {
+                name = "LD_PRELOAD";
+                value = "${pkgs.mkl}/lib/libblas.so";
+              }
+            ];
+          };
+        }
+      );
+    };
 }
